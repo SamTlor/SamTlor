bcaee1e7ad7ff0d74755483437b4566aa72d3b05
SamTlor <89541737+SamTlor@users.noreply.github.com>
2024-11-13 17:37:12 -0500

    projects have links now

--- docs/js/config.js
+++ docs/js/config.js
@@ -1,3 +1,4 @@
 var PortfolioApp = window.PortfolioApp || {};
 
+// API Gateway -> APIs -> [name of project] -> stages -> invoke url
 PortfolioApp.apiEndpoint = 'https://4wqgbacsed.execute-api.us-east-1.amazonaws.com/prod';
