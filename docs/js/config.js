10839d696e993d3383f03400d33827beffbb943f
SamTlor <89541737+SamTlor@users.noreply.github.com>
2024-11-13 19:50:17 -0500

    apiendpoint name change

--- docs/js/config.js
+++ docs/js/config.js
@@ -1,4 +1,4 @@
 var PortfolioApp = window.PortfolioApp || {};
 
 // API Gateway -> APIs -> [name of project] -> stages -> invoke url
-PortfolioApp.apiEndpoint = 'https://4wqgbacsed.execute-api.us-east-1.amazonaws.com/prod';
+PortfolioApp.apiEndpoint = 'https://u81xd75rxe.execute-api.us-east-1.amazonaws.com/prod';
