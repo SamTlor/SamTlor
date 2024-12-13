1932e47b34a45085b09ee1d193cee4adb34762a9
SamTlor <89541737+SamTlor@users.noreply.github.com>
2024-12-13 17:25:12 -0500

    I get that i could've used complex numbers but this is more me

--- python/problems/advent_of_code/2024/day6.py
+++ python/problems/advent_of_code/2024/day6.py
@@ -6,65 +6,46 @@ def find_carrot(floorplan):
         for width, cell in enumerate(row):
             if cell == "^":
                 return (height, width)
-   
-def part_one():
-    floorplan = open("day6.txt").read().strip().split("\n")
-    height, width = find_carrot(floorplan)
-  
-    guardPath = set()
-    choose_dir = 0
-    changeH, changeW = directions[choose_dir]
-    while 0 <= height + changeH < len(floorplan) and 0 <= width + changeW < len(floorplan[0]):
-        guardPath.add((height, width))
-        
-        next_height, next_width = height + changeH, width + changeW
 
-        # if the guard hits an object
-        if floorplan[next_height][next_width] == "#":
-            choose_dir = (choose_dir + 1) % len(directions)
-            changeH, changeW = directions[choose_dir]
-            continue
-
-        height, width = next_height, next_width
-
-    guardPath.add((height, width))
-    print(len(guardPath))
-    return guardPath
-            
-# working version
-def part_two():
-    ending = 0
-    floorplan = open("day6.txt").read().strip().split("\n")
-    carrotHeight, carrotWidth = find_carrot(floorplan)
+# see where the guard will go. it turns right if it hits a # or the obstacle if detect_loops is turned on
+def DoTheLoopTest(floorplan, start_height, start_width, obstacle = None, detect_loops = False):
+    path = set()
+    height, width = start_height, start_width
+    choose_dir = 0 
     
-    guardPath = part_one()
-    for row, col in guardPath:
-        
-        # reset guard starting position for every run
-        height, width = carrotHeight, carrotWidth
-        
-        # the height, width, and direction of the path the guard will take if an object is placed at row, col
-        thisPath = set()
-        choose_dir = 0
-        
-        while True:
-            space = (height, width, choose_dir)
-            if space in thisPath:
-                ending += 1
-                break
-            thisPath.add(space)
-            
-            changeH, changeW = directions[choose_dir]
-            next_height, next_width = height + changeH, width + changeW
-            
-            if not (0 <= next_height < len(floorplan) and 0 <= next_width < len(floorplan[0])):
-                break
-            
-            if (floorplan[next_height][next_width] == "#" or (next_height == row and next_width == col)):
-                choose_dir = (choose_dir + 1) % len(directions)
-            else:
-                height, width = next_height, next_width
-        
-    print(ending)
+    while True:
+        if detect_loops:
+            state = (height, width, choose_dir)
+            if state in path:
+                return path, True
+            path.add(state)
+        else:
+            path.add((height, width))
 
-part_two()
+
+        changeH, changeW = directions[choose_dir]
+        next_height, next_width = height + changeH, width + changeW
+        
+        # Check if out of bounds
+        if not (0 <= next_height < len(floorplan) and 0 <= next_width < len(floorplan[0])):
+            break
+        
+        # Handle obstacles
+        if floorplan[next_height][next_width] == "#" or (next_height, next_width) == obstacle:
+            choose_dir = (choose_dir + 1) % len(directions)
+        else:
+            height, width = next_height, next_width
+    
+    return path, False
+    
+# reading inputs
+floorplan = open("day6.txt").read().strip().split("\n")
+carrotHeight, carrotWidth = find_carrot(floorplan)
+
+# part 1
+guardPath, _ = DoTheLoopTest(floorplan, carrotHeight, carrotWidth)
+print(len(guardPath))
+
+# part 2
+print(sum(1 for space in guardPath \
+                    if DoTheLoopTest(floorplan, carrotHeight, carrotWidth, space, detect_loops=True)[1]))
