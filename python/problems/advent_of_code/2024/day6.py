f5133cca6c97c6ab094fc01aebb993c06cdd876e
SamTlor <89541737+SamTlor@users.noreply.github.com>
2024-12-13 05:47:19 -0500

    more elegant but i still want to make the while loops the same function

--- python/problems/advent_of_code/2024/day6.py
+++ python/problems/advent_of_code/2024/day6.py
@@ -6,113 +6,65 @@ def find_carrot(floorplan):
         for width, cell in enumerate(row):
             if cell == "^":
                 return (height, width)
-
-class Node:
-    def __init__(self, height, width, direction):
-        self.height = height
-        self.width = width
-        self.direction = direction
-        
-        self.prev = None
-        self.next = None
-
-class LinkedList:
-    def __init__(self):
-        self.head = None
-        self.tail = None
-        self.xCount = 0
-
-    def append(self, height, width, direction):
-        new_node = Node(height, width, direction)
-        if not self.find(height, width):
-            self.xCount += 1
-            
-        if not self.head:
-            self.head = self.tail = new_node
-        else:
-            new_node.prev = self.tail
-            self.tail.next = new_node
-            self.tail = new_node
-            
-    def find(self, height, width, direction = None):
-        current = self.tail
-        while current:
-            if (current.height == height and 
-                current.width == width and 
-                (direction is None or current.direction == direction)):
-                return True
-            current = current.prev
-        return None
-         
-    def go(self, floorplan, height, width, choose_dir):
-        changeH, changeW = directions[choose_dir % len(directions)]
-        
-        while 0 <= height + changeH < len(floorplan) and 0 <= width + changeW < len(floorplan[0]):
-            next_height = height + changeH
-            next_width = width + changeW
-
-            # if the guard hits an object
-            if floorplan[next_height][next_width] == "#":
-                return height, width, False
-
-            self.append(height, width, choose_dir)
-            height, width = next_height, next_width
-
-        self.append(height, width, choose_dir)
-        return height, width, True
-        
+   
 def part_one():
     floorplan = open("day6.txt").read().strip().split("\n")
-    guardPath = LinkedList()
-    carrotHeight, carrotWidth = find_carrot(floorplan)
+    height, width = find_carrot(floorplan)
   
+    guardPath = set()
     choose_dir = 0
-    done = False
-    while not done:
-        carrotHeight, carrotWidth, done = guardPath.go(floorplan, carrotHeight, carrotWidth, choose_dir)
-        choose_dir = (choose_dir + 1) % len(directions)
-    
-    print(guardPath.xCount)
+    changeH, changeW = directions[choose_dir]
+    while 0 <= height + changeH < len(floorplan) and 0 <= width + changeW < len(floorplan[0]):
+        guardPath.add((height, width))
+        
+        next_height, next_width = height + changeH, width + changeW
+
+        # if the guard hits an object
+        if floorplan[next_height][next_width] == "#":
+            choose_dir = (choose_dir + 1) % len(directions)
+            changeH, changeW = directions[choose_dir]
+            continue
+
+        height, width = next_height, next_width
+
+    guardPath.add((height, width))
+    print(len(guardPath))
     return guardPath
             
 # working version
 def part_two():
     ending = 0
     floorplan = open("day6.txt").read().strip().split("\n")
-    startHeight, startWidth = find_carrot(floorplan)
+    carrotHeight, carrotWidth = find_carrot(floorplan)
     
-    # guardLoc = part_one().head
-    # while guardLoc.next:
-    
-    for row in range(len(floorplan)):
-        for col in range(len(floorplan[0])):
-            height, width = startHeight, startWidth
-            direction = 0
+    guardPath = part_one()
+    for row, col in guardPath:
+        
+        # reset guard starting position for every run
+        height, width = carrotHeight, carrotWidth
+        
+        # the height, width, and direction of the path the guard will take if an object is placed at row, col
+        thisPath = set()
+        choose_dir = 0
+        
+        while True:
+            space = (height, width, choose_dir)
+            if space in thisPath:
+                ending += 1
+                break
+            thisPath.add(space)
             
-            seen = set()
-            while True:
-                # if it is somewhere that it has been before going in the same direction
-                if (height, width, direction) in seen:
-                    ending += 1
-                    break
-                    
-                seen.add((height, width, direction))
-                nextHeight, nextWidth = height + directions[direction][0], width + directions[direction][1]
-                
-                # if it leaves the map
-                if not (0 <= nextHeight < len(floorplan) and 0 <= nextWidth < len(floorplan[0])):
-                    break
-                
-                # if it hits and object
-                # first for real one second for hypothetical one
-                if (floorplan[nextHeight][nextWidth] == "#" or 
-                # (nextHeight == guardLoc.next.height and nextWidth == guardLoc.next.width)):
-                (nextHeight == row and nextWidth == col)):
-                        direction = (direction + 1) % 4
-                else:
-                    height, width = nextHeight, nextWidth
-            # guardLoc = guardLoc.next
-                
+            changeH, changeW = directions[choose_dir]
+            next_height, next_width = height + changeH, width + changeW
+            
+            if not (0 <= next_height < len(floorplan) and 0 <= next_width < len(floorplan[0])):
+                break
+            
+            if (floorplan[next_height][next_width] == "#" or (next_height == row and next_width == col)):
+                choose_dir = (choose_dir + 1) % len(directions)
+            else:
+                height, width = next_height, next_width
+        
     print(ending)
 
 part_two()
