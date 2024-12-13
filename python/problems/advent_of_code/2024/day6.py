4b8ac04aebb5a792a871a92396e6a42c83cdc1aa
SamTlor <89541737+SamTlor@users.noreply.github.com>
2024-12-12 20:14:38 -0500

    day6 works but it's inelegant D:

--- python/problems/advent_of_code/2024/day6.py
+++ python/problems/advent_of_code/2024/day6.py
@@ -1,94 +1,118 @@
+#               up    right   down   left
+directions = [(-1,0), (0,1), (1,0), (0,-1)]
+
 def find_carrot(floorplan):
     for height, row in enumerate(floorplan):
         for width, cell in enumerate(row):
             if cell == "^":
                 return (height, width)
-    return None
+
+class Node:
+    def __init__(self, height, width, direction):
+        self.height = height
+        self.width = width
+        self.direction = direction
+        
+        self.prev = None
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.xCount = 0
+
+    def append(self, height, width, direction):
+        new_node = Node(height, width, direction)
+        if not self.find(height, width):
+            self.xCount += 1
+            
+        if not self.head:
+            self.head = self.tail = new_node
+        else:
+            new_node.prev = self.tail
+            self.tail.next = new_node
+            self.tail = new_node
+            
+    def find(self, height, width, direction = None):
+        current = self.tail
+        while current:
+            if (current.height == height and 
+                current.width == width and 
+                (direction is None or current.direction == direction)):
+                return True
+            current = current.prev
+        return None
+         
+    def go(self, floorplan, height, width, choose_dir):
+        changeH, changeW = directions[choose_dir % len(directions)]
+        
+        while 0 <= height + changeH < len(floorplan) and 0 <= width + changeW < len(floorplan[0]):
+            next_height = height + changeH
+            next_width = width + changeW
+
+            # if the guard hits an object
+            if floorplan[next_height][next_width] == "#":
+                return height, width, False
+
+            self.append(height, width, choose_dir)
+            height, width = next_height, next_width
+
+        self.append(height, width, choose_dir)
+        return height, width, True
         
 def part_one():
-    
-    with open("day6.txt") as f:
-        floorplan = [list(line.strip()) for line in f if line.strip()]
-    height, width = find_carrot(floorplan)
-    
-    
-    
-    def go(floorplan, height, width, changeHeight, changeWidth):
-        while 0 <= height + changeHeight < len(floorplan) and 0 <= width + changeWidth < len(floorplan[0]):
-            next_height = height + changeHeight
-            next_width = width + changeWidth
-
-            if floorplan[next_height][next_width] == "#":
-                return floorplan, height, width, False
-
-            floorplan[height][width] = "X"
-            height, width = next_height, next_width
-
-        floorplan[height][width] = "X"
-        return floorplan, height, width, True
-
-    
-    
-    direction = [(-1,0), (0,1), (1,0), (0,-1)]
+    floorplan = open("day6.txt").read().strip().split("\n")
+    guardPath = LinkedList()
+    carrotHeight, carrotWidth = find_carrot(floorplan)
+  
     choose_dir = 0
     done = False
     while not done:
-        dh, dw = direction[choose_dir % len(direction)]
-        floorplan, height, width, done = go(floorplan, height, width, dh, dw)
-        choose_dir += 1
+        carrotHeight, carrotWidth, done = guardPath.go(floorplan, carrotHeight, carrotWidth, choose_dir)
+        choose_dir = (choose_dir + 1) % len(directions)
     
-    print(sum(row.count("X") for row in floorplan))
-
-def part_two():
-    with open("day6.txt") as f:
-        floorplan = [list(line.strip()) for line in f if line.strip()]
-
-    height, width = find_carrot(floorplan)
-    
-    
-    # TODO: something about the path from two #'s ago 
-    # like if it's the same row or column as the one from two #'s ago put an O there idk
-    # new if statement new return
-    direction = [(-1,0), (0,1), (1,0), (0,-1)]
-    def go_two(floorplan, height, width, choose_dir, path):
-        changeHeight, changeWidth = direction[choose_dir % len(direction)]
-        
-        while 0 <= height + changeHeight < len(floorplan) and 0 <= width + changeWidth < len(floorplan[0]):
-            next_height = height + changeHeight
-            next_width = width + changeWidth
-
-            if floorplan[next_height][next_width] == "#":
-                path.append((next_height, next_width))
-                if len(path) > 3:
-                    path.pop(0)
-                return floorplan, height, width, False, path, False
-
-            testChangeHeight, testChangeWidth = direction[(choose_dir + 1) % len(direction)]
+    print(guardPath.xCount)
+    return guardPath
             
-            testHeight, testWidth = height + testChangeHeight, width + testChangeWidth
-            if floorplan[testHeight][testWidth] == path[0]:
-                return floorplan, height, width, False, path, True
-            floorplan[height][width] = "X"
-            height, width = next_height, next_width
-
-        floorplan[height][width] = "X"
-        return floorplan, height, width, True, path, False
-
-    
-    
-    choose_dir = 0
-    done = False
-    path = []
+# working version
+def part_two():
     ending = 0
-    while not done:
-        floorplan, height, width, done, path, loop = go_two(floorplan, height, width, choose_dir, path)
-        if loop:
-            ending += 1
-        choose_dir += 1
-        loop = False
+    floorplan = open("day6.txt").read().strip().split("\n")
+    startHeight, startWidth = find_carrot(floorplan)
     
+    # guardLoc = part_one().head
+    # while guardLoc.next:
+    
+    for row in range(len(floorplan)):
+        for col in range(len(floorplan[0])):
+            height, width = startHeight, startWidth
+            direction = 0
+            
+            seen = set()
+            while True:
+                # if it is somewhere that it has been before going in the same direction
+                if (height, width, direction) in seen:
+                    ending += 1
+                    break
+                    
+                seen.add((height, width, direction))
+                nextHeight, nextWidth = height + directions[direction][0], width + directions[direction][1]
+                
+                # if it leaves the map
+                if not (0 <= nextHeight < len(floorplan) and 0 <= nextWidth < len(floorplan[0])):
+                    break
+                
+                # if it hits and object
+                # first for real one second for hypothetical one
+                if (floorplan[nextHeight][nextWidth] == "#" or 
+                # (nextHeight == guardLoc.next.height and nextWidth == guardLoc.next.width)):
+                (nextHeight == row and nextWidth == col)):
+                        direction = (direction + 1) % 4
+                else:
+                    height, width = nextHeight, nextWidth
+            # guardLoc = guardLoc.next
+                
+    print(ending)
 
-
-
-part_one()
 part_two()
