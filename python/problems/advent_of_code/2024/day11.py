fac6af2a80051302685896876211ac88bc817a72
SamTlor <89541737+SamTlor@users.noreply.github.com>
2024-12-15 17:06:41 -0500

    day 7 spacing

--- python/problems/advent_of_code/2024/day11.py
+++ python/problems/advent_of_code/2024/day11.py
@@ -1,20 +1,46 @@
-thing = 123956
-print(thing // 1000)
-print(thing % 1000)
-
+num_lookup = {}
 
 def helper(nums, iteration):
-    new_nums = []
-    
-    for num in nums:
-        if num == 0:
-            new_nums.append(1)
+    if iteration < 25:
+        new_nums = []
         
-        num_len = len(str(num))
-        if num_len % 2 == 0:
-            new_nums.append(num // (10 ** (num_len // 2)))
-            new_nums.append(num % (10**(num_len // 2) ))
-
+        for num in nums:
+            num_len = len(str(num))
+            
+            if num == 0:
+                new_nums.append(1)
+            elif num_len % 2 == 0:
+                new_nums.append(num // (10 ** (num_len // 2)))
+                new_nums.append(num % (10**(num_len // 2)))
+            else:
+                new_nums.append(num * 2024)
+        return helper(new_nums, iteration + 1)
+        
+        
+        
+    else:
+        return len(nums)
 
 def part_one():
+    nums = [int(num) for line in open("day11.txt") for num in line.split()]
+    print(helper(nums, 0))
+
+
+def helper_two(nums, iteration):
+    if iteration < 25:
+        new_nums = []
+        
+        for num in nums:
+            if num in num_lookup:
+                new_nums += num_lookup[num]
+                
+            else:
+                
+        return helper_two(new_nums, iteration + 1)
     
+def part_two():
+    nums = [int(num) for line in open("day11.txt") for num in line.split()]
+    print(helper_two(nums, 0))
+
+part_one()
+part_two()
