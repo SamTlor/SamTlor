78de85589d6f99b709876ef65d3f3f9306fc13d6
SamTlor <89541737+SamTlor@users.noreply.github.com>
2024-12-16 00:59:14 -0500

    day 11 done dynamic programming

--- python/problems/advent_of_code/2024/day11.py
+++ python/problems/advent_of_code/2024/day11.py
@@ -1,46 +1,49 @@
-num_lookup = {0: 1}
+num_count = {}
 
-def helper(nums, iteration):
-    if iteration < 25:
-        new_nums = []
-        
-        for num in nums:
-            num_len = len(str(num))
-            
-            if num == 0:
-                new_nums.append(1)
-            elif num_len % 2 == 0:
-                new_nums.append(num // (10 ** (num_len // 2)))
-                new_nums.append(num % (10 ** (num_len // 2)))
-            else:
-                new_nums.append(num * 2024)
-        return helper(new_nums, iteration + 1)
-        
-        
-        
+def add_count(val, count):
+    if val in num_count:
+        num_count[val] += count
     else:
-        return len(nums)
+        num_count[val] = count
 
-def part_one():
-    nums = [int(num) for line in open("day11.txt") for num in line.split()]
-    print(helper(nums, 0))
+def recursive_helper(num_count, iteration = 75):
+    # Base case: stop recursion
+    if iteration == 0:
+        return sum(num_count.values())
 
+    # Temporary dictionary to store changes
+    temp_count = {}
 
-def helper_two(nums, iteration):
-    if iteration < 50:
-        new_nums = []
-        
-        for num in nums:
-            if num in num_lookup:
-                new_nums += num_lookup[num]
-                
-            else:
-                num_lookup[num] = 
-        return helper_two(new_nums, iteration + 1)
-    
-def part_two():
-    nums = [int(num) for line in open("day11.txt") for num in line.split()]
-    print(helper_two(nums, 0))
+    for stone, count in num_count.items():
+        if count == 0:
+            continue
 
-part_one()
-part_two()
+        num_len = len(str(stone))
+
+        # 0 -> 1
+        if stone == 0:
+            temp_count[1] = temp_count.get(1, 0) + count
+
+        # Even number -> split number
+        elif num_len % 2 == 0:
+            left = stone // (10 ** (num_len // 2))
+            right = stone % (10 ** (num_len // 2))
+            
+            temp_count[left] = temp_count.get(left, 0) + count
+            temp_count[right] = temp_count.get(right, 0) + count
+
+        # Else -> multiply by 2024
+        else:
+            multiplied = stone * 2024
+            temp_count[multiplied] = temp_count.get(multiplied, 0) + count
+
+    # Recursive call with updated counts and decremented iteration
+    return recursive_helper(temp_count, iteration - 1)
+
+# Initialize num_count
+nums = [int(num) for line in open("day11.txt") for num in line.split()]
+for i in nums:
+    add_count(i, 1)
+
+# Call the recursive helper
+print(recursive_helper(num_count))
