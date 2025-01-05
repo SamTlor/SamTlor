ddbae68c622db7d252301902eb4e190d3df1388d
SamTlor <89541737+SamTlor@users.noreply.github.com>
2025-01-05 16:42:46 -0500

    day 12 bfs

--- python/problems/advent_of_code/2024/day12.py
+++ python/problems/advent_of_code/2024/day12.py
@@ -1,5 +1,8 @@
 from collections import deque
 
+#               up    right   down   left
+directions = [(-1,0), (0,1), (1,0), (0,-1)]
+
 # Reading inputs
 grid = [[char for char in line] 
          for line in open("day12.txt").read().strip().split("\n")]
@@ -14,18 +17,69 @@ def isValid(height, width):
 def bfs(height, width, value):
     queue = deque([(height, width)])
     visited.add((height, width))
-    perimeter = 0
+    perimeter, area = 0, 0
     
     while queue:
         thisH, thisW = queue.popleft()
+        area += 1
         
-        for changeH, changeW in []
-    
+        for changeH, changeW in directions:
+            newH, newW = thisH + changeH, thisW + changeW
+            
+            if not isValid(newH, newW) or grid[newH][newW] != value:
+                perimeter += 1
+            elif (newH, newW) not in visited:
+                visited.add((newH, newW))
+                queue.append((newH, newW))
+                
+    return perimeter, area
     
 def part_one():
     for i in range(maxHeight):
         for j in range(maxWidth):
             if (i, j) not in visited:
                 value = grid[i][j]
-                perimeters[value] = perimeters.get(value, 0) + bfs(i, j, value)
-                print(perimeters[value])
+                group_perim, group_area = bfs(i, j, value)
+                perimeters[value] = perimeters.get(value, 0) + group_perim * group_area
+                
+                
+                
+    ending = 0
+    for i in perimeters:
+        ending += perimeters[i]
+        
+    print(ending)
+
+
+
+sides = {}
+sides_visited = set()
+def bfs_two(height, width, value):
+    queue = deque([(height, width)])
+    sides_visited.add((height, width))
+    side_count, area = 0, 0
+    
+    while queue:
+        thisH, thisW = queue.popleft()
+        area += 1
+        
+        for changeH, changeW in directions:
+            newH, newW = thisH + changeH, thisW + changeW
+            
+            if not isValid(newH, newW) or grid[newH][newW] != value:
+                side_count += 1
+            elif (newH, newW) not in sides_visited:
+                sides_visited.add((newH, newW))
+                queue.append((newH, newW))
+    return side_count, area
+
+    
+    
+    
+    
+    
+    
+    
+    
+          
+part_one()
