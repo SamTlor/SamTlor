bfc007206ce6a3982331c545f02b50a7024503b1
SamTlor <89541737+SamTlor@users.noreply.github.com>
2025-01-13 20:53:35 -0500

    typescript portfolio website

--- typescript/portfolio/src/app/about/page.tsx
+++
@@ -1,5 +0,0 @@
-function About(){
-    return <div id = "about" className="flex justify-center items-center bg-gray-500 h-screen text-white">About</div>
-}
-
-export default About;
---
+++ typescript/portfolio_real_copy/src/static/Project.tsx
@@ -0,0 +1,25 @@
+import Tag from "./Tag";
+
+function Project ({ headline, image, text, tags, link }: { headline: string, image: string, text: string, tags: string[], link: string }) {
+    return (
+        <a href={link} aria-label={text}>
+            <div className="flex flex-col transition-all hover:scale-105 scale-100 bg-opacity-20 bg-black rounded-lg max-h-max max-w-96 m-3 text-white">
+                <div className="w-full h-64 bg-black rounded-t-lg flex justify-center items-center">
+                    <img className="w-full h-full object-contain" alt={text} src={image} />
+                </div>
+                
+                <div className="p-4">
+                    <div className="text-xl font-medium mb-4">{headline}</div>
+                    <p>{text}</p>
+                    <ul className="mt-2 flex flex-wrap" aria-label="Technologies used:">
+                        {tags.map((item, index) =>
+                            <li key={index} className="mr-1.5 mt-2"><Tag title={item} /></li>
+                        )}
+                    </ul>
+                </div>
+            </div>
+        </a>
+    );
+}
+
+export default Project;
