aaba2eb1a0010ecfe9aca25be3a50db81aaff5c8
SamTlor <89541737+SamTlor@users.noreply.github.com>
2025-01-17 17:41:39 -0500

    no more apostrophes

--- docs/src/static/Summary.tsx
+++ docs/src/static/Summary.tsx
@@ -4,8 +4,8 @@ function Summary(){
         <p className="text-4xl mb-8">About me</p>
         <article className="desktop:text-xl mobile:text-md" itemScope >
             <p className="mb-5">
-                I'm a fullstack developer that just graduated with a bachelors degree in Computer Science. I 
-                gained experience from an internship and a full time job. So far I've worked on prompt 
+                I&apos;m a fullstack developer that just graduated with a bachelors degree in Computer Science. I 
+                gained experience from an internship and a full time job. So far I&apos;ve worked on prompt 
                 engineering for custom AI bots, integrating APIs to safguard sensitive information, and 
                 implementing AWS services. 
             </p>
