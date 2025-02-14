ddd964fd8113a806c2a52cfbd1781d71b4150d34
SamTlor <89541737+SamTlor@users.noreply.github.com>
2025-02-13 21:21:34 -0500

    wording fixes

--- typescript/portfolio/src/app/projects/BoxOfficeMaxTvDemo/page.tsx
+++ typescript/portfolio/src/app/projects/BoxOfficeMaxTvDemo/page.tsx
@@ -76,7 +76,6 @@ export default function BoxOfficeMaxTvDemo() {
                         <option value="Directors">Directors</option>
                         <option value="Movies">Movies</option>
                         <option value="Studios">Studios</option>
-                        <option value="projects">projects</option>
                     </select>
 
                     <button type="submit" className="mt-4 px-6 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-700">
