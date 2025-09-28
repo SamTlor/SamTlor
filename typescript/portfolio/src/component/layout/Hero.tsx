d0c20e519414dc686445463f45f187426bfa169c
SamTlor <89541737+SamTlor@users.noreply.github.com>
2025-09-28 01:12:01 -0400

    navbar font changes and greetings font change

--- typescript/portfolio/src/component/layout/Hero.tsx
+++ typescript/portfolio/src/component/layout/Hero.tsx
@@ -10,22 +10,22 @@ function Hero() {
     return <>
         <div className="flex items-center flex-col min-h-screen desktop:px-20 mobile:p-6 bg-gradient-to-r from-gradientLeft to-gradientRight">
             <div className="flex mobile:justify-center desktop:flex-row desktop:items-center mobile:flex-col-reverse items-center w-full flex-1">
-                    <div className="flex flex-col z-20 pointer-events-none text-white desktop:w-3/5 mobile:w-full mobile:text-center">
-                        <p className="desktop:text-[5vw] mobile:text-[8vw] font-bold text-aqua w-full">Greetings, I&apos;m Sam</p>
-                        <p className="desktop:text-[3vw] mobile:text-[5vw]">a developer with experience in <Typing /></p>
-                        
-                        <div className='flex mobile:hidden mt-6 justify-start w-[17vw]'><SocialMedia size={200} /></div>
-                    </div>
+                <div className="flex flex-col z-20 pointer-events-none text-white desktop:w-3/5 mobile:w-full mobile:text-center font-sans">
+                    <p className="desktop:text-[5vw] mobile:text-[8vw] font-bold text-lightcyan w-full">Greetings, I&apos;m Sam</p>
+                    <p className="desktop:text-[3vw] mobile:text-[5vw]">a developer with experience in <Typing /></p>
+                    
+                    <div className='flex mobile:hidden mt-6 justify-start w-[17vw]'><SocialMedia size={200} /></div>
+                </div>
 
-                    <div className="relative w-1/5mobile:w-full mobile:max-w-xl self-center rounded-full overflow-hidden border-4 border-[#84ffff]">
-                        <Image  
-                            src="/images/samPicture.jpg" 
-                            alt="Portrait"
-                            width={300}
-                            height={500}
-                            className="object-cover animate-scalepulse"
-                        />
-                    </div>
+                <div className="relative w-1/5mobile:w-full mobile:max-w-xl self-center rounded-full overflow-hidden border-4 border-[#84ffff]">
+                    <Image  
+                        src="/images/samPicture.jpg" 
+                        alt="Portrait"
+                        width={300}
+                        height={500}
+                        className="object-cover animate-scalepulse"
+                    />
+                </div>
 
 
                 </div>
