fa71aceaa292eefd70853306a8713f390dd3c7c6
SamTlor <89541737+SamTlor@users.noreply.github.com>
2025-09-28 01:34:39 -0400

    greetings background and about font

--- typescript/portfolio/src/static/Timeline.tsx
+++ typescript/portfolio/src/static/Timeline.tsx
@@ -8,13 +8,15 @@ function Timeline (){
       {
         timelineData.data.map((item, index) => <div key={"W" + index} className="flex">
           <div>
-            <div className="flex justify-center items-center text-white rounded-full w-20 h-20 bg-slate-600">
-              <div className="flex h-[72px] w-[72px] bg-slate-800 font-bold rounded-full justify-center items-center">{item.year.toString()}</div>
+            <div className="flex justify-center items-center text-white rounded-full w-20 h-20 bg-slate-600 font-display">
+              <div className="flex h-[72px] w-[72px] bg-slate-800 font-bold rounded-full justify-center items-center">
+                {item.year.toString()}
+              </div>
             </div>
             {index < timelineData.data.length - 1 ? <div className=" w-1 bg-slate-600 m-auto h-full"></div> : <div className="h-16"></div>}
           </div>
           
-          <div className="flex flex-col justify-start h-max mt-5 px-6 text-white">
+          <div className="flex flex-col justify-start h-max mt-5 px-6 text-white font-sans">
             <div className="font-bold">{item.title + " @ "}<span className="text-[#84ffff] font-bold">{item.company}</span></div>
             <div>{item.tasks}</div>
             <ul className="mt-1 flex flex-wrap" aria-label="Technologies used:">
