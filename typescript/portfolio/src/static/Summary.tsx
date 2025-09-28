b52009801693b6960007cb1f6f5bde0caf9c836f
SamTlor <89541737+SamTlor@users.noreply.github.com>
2025-09-28 01:24:33 -0400

    cosmos at beginning and font changes

--- typescript/portfolio/src/static/Summary.tsx
+++ typescript/portfolio/src/static/Summary.tsx
@@ -1,11 +1,11 @@
 
 function Summary(){
     return (<>
-        <p className="text-4xl mb-8 bg-[#84ffff] text-black px-4 py-2 rounded-full inline">
+        <p className="text-4xl mb-8 bg-[#84ffff] text-black px-4 py-2 rounded-full inline font-display">
             About Me
         </p>
 
-        <article className="desktop:text-xl mobile:text-md" itemScope >
+        <article className="desktop:text-xl mobile:text-md font-sans" itemScope >
             <p className="mb-5">
                 I&apos;m a full stack developer with experience in a lot of different fields.
                 I&apos;ve worked on prompt engineering for custom AI bots, integrating APIs to safeguard 
