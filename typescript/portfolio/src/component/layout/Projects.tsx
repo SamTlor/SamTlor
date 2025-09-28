750534461990d00a56ff337494582d8d61476bb7
SamTlor <89541737+SamTlor@users.noreply.github.com>
2025-09-28 13:11:54 -0400

    different colors different sections

--- typescript/portfolio/src/component/layout/Projects.tsx
+++ typescript/portfolio/src/component/layout/Projects.tsx
@@ -6,33 +6,35 @@ export default function Projects() {
     
     return (
         <>
-        <div id="Projects" className="bg-gradient-to-r from-gradientLeft to-gradientRight">
-            <div className="group-mobile">
+            <div id="Projects" className="bg-aqua">
                 <img className="w-full h-34" src="/transitions/transition_grey.svg" alt="transition" />
+                <p className="flex text-4xl backdrop-blur-xs p-6">Featured Projects</p>
+                <div className="flex flex-wrap w-full desktop:px-20 justify-center">
+                    {getProjects()
+                    .filter((project) => project.metadata.title != "NotFound")
+                    .map((project) =>
+                        <Project
+                            key = {project.metadata.id}
+                            title = {project.metadata.title} 
+                            image = {project.metadata.image} 
+                            text = {project.metadata.text} 
+                            tags = {project.metadata.tags} 
+                            slug = {project.slug} 
+                        />
+                    )}
+                </div>
             </div>
-            <p className="flex text-4xl my-8 justify-center backdrop-blur-xs shadow-glow text-white font-bold">Featured Projects</p>
-            <div className="flex flex-wrap w-full desktop:px-20 justify-center">
-                {getProjects()
-                .filter((project) => project.metadata.title != "NotFound")
-                .map((project) =>
-                    <Project
-                        key = {project.metadata.id}
-                        title = {project.metadata.title} 
-                        image = {project.metadata.image} 
-                        text = {project.metadata.text} 
-                        tags = {project.metadata.tags} 
-                        slug = {project.slug} 
+
+            {/* transitions to 'you have a job opening' */}
+            <div className="relative w-full h-34 bg-aqua overflow-hidden">
+                <div className="group-mobile">
+                    <img 
+                        src="/transitions/transition_grey.svg" 
+                        alt="transition" 
+                        className="object-cover w-full h-full desktop:scale-y-[-1] block"
                     />
-                )}
+                </div>
             </div>
-        </div>
-        <div className="relative w-full h-34 bg-gradient-to-r from-gradientLeft to-gradientRight overflow-hidden">
-            <img 
-                src="/transitions/transition_grey.svg" 
-                alt="transition" 
-                className="object-cover w-full h-full desktop:scale-y-[-1] block"
-            />
-        </div>
         </ >
     );
 }
